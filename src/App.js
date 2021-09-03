9e7e33910388deb5e58652721679196c247bf453
Thomas Sankara <thomassankara74@gmail.com>
2021-09-03 08:29:18 +0300

    main component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Attribution, Hero, Jobs } from "./components"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <main className="container mx-auto">
+        <Hero />
+        <Jobs />
+        <Attribution />
+      </main>
+    </>
+  )
 }
 
-export default App;
+export default App
